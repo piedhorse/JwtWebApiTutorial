8244b6db602bee5392cc570749a95ce1c6152fcb
piedhorse <umutalacatli@icloud.com>
2023-01-21 22:08:56 +0300

    Proje dosyası ekle.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] PasswordHash, out byte[] PasswordSalt);
+            user.Username = request.Username;
+            user.PasswordHash = PasswordHash;
+            user.PasswordSalt = PasswordSalt;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("Kullanici Bulunamadi");
+            }
+
+            if (!VerifyPasswordHash(request.Password,user.PasswordHash,user.PasswordSalt))
+            {
+                return BadRequest("Yanlis sifre girildi");
+
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.Username)
+            };
+            var key=new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+               _configuration.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                
+                claims: claims,
+                expires:DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+
+            var jwt= new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+        private void CreatePasswordHash(string Password, out byte[] PasswordHash, out byte[] PasswordSalt)
+        {
+
+
+            using (var hmac = new HMACSHA512())
+            {
+
+                PasswordSalt = hmac.Key;
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password));
+
+            }
+
+        }
+        private bool VerifyPasswordHash(string Password,  byte[] PasswordHash,  byte[] PasswordSalt)
+        {
+            using (var hmac = new HMACSHA512(PasswordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Password));
+
+                return computeHash.SequenceEqual(PasswordHash);
+            }
+        }
+    }
+}
